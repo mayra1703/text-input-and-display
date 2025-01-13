c611c00451025d27e7ee003c84af66d973e180da
Mayra <maymendez3666@gmail.com>
2025-01-12 22:00:23 -0800

    first commit

---
+++ app/src/main/java/com/example/project03/MainActivity.kt
@@ -0,0 +1,12 @@
+// Name Project: Text Input And Display
+package com.example.project03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
